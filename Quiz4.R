8b50fa93c5cf7bbd90f31134f7e4cf4c81b749b4
lecroc <clecrone@compasspoint-media.com>
2017-07-23 17:40:47 -0500

    Final Quiz 4

--- Quiz4.R
+++ Quiz4.R
@@ -16,9 +16,9 @@ vector<-names(Q1dat) # create a vector that is the names of the variables
 
 list<-strsplit(vector, "wgtp") # apply strsplit() to generate a list
 
-answer<-list[123] # select the 123rd item on the list
+answer1<-list[123] # select the 123rd item on the list
 
-answer  # display the ansser
+answer1  # display the ansser
 
 
 #2
@@ -41,8 +41,68 @@ vector21<-gsub(",", "", vector2) # substitute "" for each ","
 
 vector21<-as.numeric(vector21) # convert vector21 to numbers
 
-answer<-mean(vector21) # calculate the mean
+answer2<-mean(vector21) # calculate the mean
 
-answer # diplay the answer
+answer2 # diplay the answer
+
+
+#3
+
+countryNames<-GDP$CountryName  # create a vector of the country names
+
+answer3<-grep("^United", countryNames)  # this is the correct regex code for grep
+
+answer3a<-length(answer3) # count how many countries match the regex
+
+answer3a # display the number of countries
+
+answer3 # display the position of the countries in data frame GDP
+
+View(GDP) # look at GDP to make sure the referenced rows' country names start with "United"
+
+
+#4
+
+# Get new data
+
+fileUrl44<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+
+download.file(fileUrl44, destfile = "./Country.csv")
+
+EDU<-read.csv("Country.csv") # read in second table to EDU
+
+mrg<-merge(GDP, EDU, by="CountryCode") # merges on CountryCode as only similar column in both tables
+
+answer4<-length(grep("*Fiscal year end: June", mrg$Special.Notes)) # store length of vector of matching strings
+
+answer4 # dispay answer
+
+
+#5
+
+library(quantmod)
+library(lubridate)
+
+amzn = getSymbols("AMZN",auto.assign=FALSE) # get data on Amazon stock prices
+
+sampleTimes = index(amzn) # store times from amzn into a vector
+
+st<-as.data.frame(sampleTimes) # convert vector to data frame
+
+names(st)<-c("Date")  # give the column in st the name of "Date"
+
+st1<-subset(st, year(st$Date)==2012)  # subset the data frame for year= 2012
+
+answer5a<-nrow(st1)  # store the number of rows into answer5a
+
+st2<-subset(st, year(st$Date)==2012 & wday(st$Date)==2) # subset st for year=2012 and weekday = Monday (2)
+
+answer5b<-nrow(st2)  # store the number of rows into answer 5b
+
+answer5a  # display number of values collected in 2012
+
+answer5b  # display number of values collected in 2012 on a Monday
+
+
 
 
